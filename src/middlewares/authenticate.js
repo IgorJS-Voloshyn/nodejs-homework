61bcda8873e3bdeac85e3e422059737730710b8e
IgorJS-Voloshyn <igorVoloshyn.js@gmail.com>
2025-02-02 20:22:50 +0200

    fix authenticate

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -4,6 +4,7 @@ import { User } from '../models/user.js';
 import { Session } from '../models/session.js';
 
 export async function authenticate(req, res, next) {
+  const [bearer, accessToken] = req.headers.authorization.split(' ', 2);
   const session = await Session.findOne({ accessToken });
 
   if (new Date() > new Date(session.accessTokenValidUntil)) {
@@ -11,7 +12,6 @@ export async function authenticate(req, res, next) {
   }
 
   const user = await User.findOne({ _id: session.userId });
-
   req.user = user;
 
   next();
